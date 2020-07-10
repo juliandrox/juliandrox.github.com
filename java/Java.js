78e62c7000b581b15a81a8788394891d11070bda
Julian <ju3324452@hotmail.com>
2020-07-09 21:34:55 -0500

    new
    java + css

---
+++ java/Java.js
@@ -0,0 +1,17 @@
+
+
+console.log("Hola desde un archivo externo de Java Script");
+
+//Para seleccionar los elementos o etiquetas HTML
+let links= document.querySelectorAll(".class");
+//console.log(links);
+//Para recorrer los elementos o etiquetas HTML
+links.forEach(function(link){
+  //console.log(link);
+  //Para dar una acción al elemento seleccionado  User and Script de programación
+ links.addEventListener("click", function(){
+console.log("Face");
+//Case Sensitive
+ });
+
+})
